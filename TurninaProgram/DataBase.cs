b6635a2aefaf2225889092b24dcb636a8eea68a4
2rnina <95979748+2rnina@users.noreply.github.com>
2021-12-22 12:59:38 +0300

    Add files via upload

---
+++ TurninaProgram/DataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurninaProgram
+{
+    class DataBase
+    {
+        public static ToursEntities Context { get; set; }
+    }
+}
---
+++ TurninaProgram/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TurninaProgram
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataBase.Context = new ToursEntities();
+
+            MainFrame.Navigate(new ToursPage());
+        }
+
+        public void firstUploadOfPhotos()
+        {
+            string path = "C:\\Users\\KIT_07_206\\Desktop\\турнина\\import\\Туры фото";
+            List<string> Files = Directory.GetFiles(path).ToList();
+
+            foreach (string FilePath in Files)
+            {
+                using (FileStream stream = File.OpenRead(FilePath))
+                {
+                    byte[] FileBytes = new byte[stream.Length];
+                    stream.Read(FileBytes, 0 ,FileBytes.Length);
+
+                    string FileName = System.IO.Path.GetFileNameWithoutExtension(FilePath);
+                    Tour RedactingTour = DataBase.Context.Tour.Where(finding => finding.Name == FileName).FirstOrDefault();
+                    RedactingTour.ImagePreview = FileBytes;
+                    DataBase.Context.SaveChanges();
+                }
+            }
+
+            MessageBox.Show("Check database");
+        }
+    }
+}
